8d36fcdd5d2baf5065af3c2ac057663ef94eac29
RafaelHDSV <rafaebolt@gmail.com>
2023-02-18 11:56:31 -0300

    Updating app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import { useState, UseEffect } from 'react'
+import { BsTrash, BsBookmarkCheck, BsBookmarkCheckFill } from 'react-icons/bs'
+
 import './App.css';
 
+const API = 'http://localhost:5000'
+
 function App() {
+  const [title, setTitle] = useState('')
+  const [time, setTime] = useState('')
+  const [todos, setTodos] = useState([])
+  const [loading, setLoading] = useState(false)
+
   return (
     <div className="App">
+      <div className="todo_header">
+        <h1>React Todo</h1>
+      </div>
 
+      <div className="form_todo">
+        <p>Form</p>
+      </div>
+
+      <div className="list_todo">
+        <p>List</p>
+      </div>
     </div>
   );
 }
