b7491b9d016db2d79dd229625acc6fdd2fcbfda7
RafaelHDSV <rafaebolt@gmail.com>
2023-02-18 11:58:52 -0300

    Updating app.js

--- src/App.js
+++ src/App.js
@@ -11,6 +11,10 @@ function App() {
   const [todos, setTodos] = useState([])
   const [loading, setLoading] = useState(false)
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
   return (
     <div className="App">
       <div className="todo_header">
@@ -18,11 +22,16 @@ function App() {
       </div>
 
       <div className="form_todo">
-        <p>Form</p>
+        <h2>Insira a sua próxima tarefa:</h2>
+
+        <form onSubmit={handleSubmit}>
+          <input type="submit" value="Enviar" />
+        </form>
       </div>
 
       <div className="list_todo">
-        <p>List</p>
+        <h2>Lista de Tarefas: </h2>
+        {todos.length === 0 && (<p>Não há tarefas</p>)}
       </div>
     </div>
   );
